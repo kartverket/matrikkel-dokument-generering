e6024f92db1575f3435d37b13cae7a8439def452
fredrir <fhansteen@gmail.com>
2026-06-25 14:18:01 +0200

    satt opp Caddy oppsett

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const useMock = env.VITE_USE_MOCK === "true";
+
+  return {
+    plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+    server: {
+      proxy: useMock
+        ? undefined
+        : {
+            "/api": {
+              target: "http://0.0.0.0:8086",
+              changeOrigin: true,
+            },
+          },
+    },
+  };
+});
