baa24201bf87eb7a3831763f8ff4915fa5b964b2
fredrir <fhansteen@gmail.com>
2026-06-26 16:47:00 +0200

    fjerne api-proxy og la til skip configurasjon

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 import babel from "@rolldown/plugin-babel";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const useMock = env.VITE_USE_MOCK === "true";
-
-  return {
-    plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-    server: {
-      proxy: useMock
-        ? undefined
-        : {
-            "/api": {
-              target: "http://0.0.0.0:8086",
-              changeOrigin: true,
-            },
-          },
-    },
-  };
+export default defineConfig({
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
