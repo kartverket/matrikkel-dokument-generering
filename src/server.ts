ef966fcb56ed48fa265d3f2bdadbffbc2953ae35
fredrir <fhansteen@gmail.com>
2026-06-26 17:11:12 +0200

    Benytte heimdall-deploy + versionering i deployment pipeline

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,20 @@ const server = Bun.serve({
   port,
   async fetch(req) {
     const url = new URL(req.url);
+
+    if (
+      url.pathname === "/internal/isAlive" ||
+      url.pathname === "/internal/isReady"
+    ) {
+      return new Response("OK");
+    }
+
     const pathname = url.pathname === "/" ? "/index.html" : url.pathname;
 
     const file = Bun.file(`${distDir}${pathname}`);
     if (await file.exists()) {
       return new Response(file);
     }
-
     return new Response("Not Found", { status: 404 });
   },
 });
