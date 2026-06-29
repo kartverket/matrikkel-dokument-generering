645715bdef3eb3954f56a556e8b41de2e9f1ab08
fredrir <fredrik.hansteen@kartverket.no>
2026-06-29 13:50:34 +0200

    Formatering og lint fix

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,27 @@
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 3000
 
-const distDir = "./dist";
+const distDir = "./dist"
 
 const server = Bun.serve({
   port,
   async fetch(req) {
-    const url = new URL(req.url);
+    const url = new URL(req.url)
 
     if (
       url.pathname === "/internal/isAlive" ||
       url.pathname === "/internal/isReady"
     ) {
-      return new Response("OK");
+      return new Response("OK")
     }
 
-    const pathname = url.pathname === "/" ? "/index.html" : url.pathname;
+    const pathname = url.pathname === "/" ? "/index.html" : url.pathname
 
-    const file = Bun.file(`${distDir}${pathname}`);
+    const file = Bun.file(`${distDir}${pathname}`)
     if (await file.exists()) {
-      return new Response(file);
+      return new Response(file)
     }
-    return new Response("Not Found", { status: 404 });
+    return new Response("Not Found", { status: 404 })
   },
-});
+})
 
-console.log(`Server running on http://localhost:${server.port}`);
+console.log(`Server running on http://localhost:${server.port}`)
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import babel from "@rolldown/plugin-babel";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite'
-
+import babel from "@rolldown/plugin-babel"
+import tailwindcss from "@tailwindcss/vite"
+import react, { reactCompilerPreset } from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss(),
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
   ],
-});
+})
