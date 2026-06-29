1114f8de90bea901230fc2b929e52277ad67e714
olalellingsen <ola.l.ellingsen@gmail.com>
2026-06-29 16:21:42 +0200

    fjernet tailwind regel og kjørte formatting
    Signed-off-by: olalellingsen <ola.l.ellingsen@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-
 function App() {
-
-  return (
-    <section className="bg-red-200">
-      matrikkel-dokument-generering
-    </section>
-  )
+  return <section className="bg-red-200">matrikkel-dokument-generering</section>
 }
 
 export default App
