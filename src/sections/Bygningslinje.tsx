4dd49175a0399335e8cd3ed38e87d45c9e6d9484
fredrir <fredrik.hansteen@kartverket.no>
2026-07-09 13:34:46 +0200

    refaktor

--- src/sections/Bygningslinje.tsx
+++ src/sections/Bygningslinje.tsx
@@ -1,6 +1,6 @@
 import { Table } from "@kv-designsystem/react"
 import { useTranslation } from "react-i18next"
-import type { Bygning } from "../lib/schema/byggRapportSchema"
+import type { Bygning, Bygningsendring } from "../lib/schema/byggRapportSchema"
 import { formatArea } from "../lib/utils/format"
 
 interface Props {
@@ -9,12 +9,38 @@ interface Props {
 
 const SEFRAK_KATEGORI = "SEFRAK-registrert bygning"
 
+function jaNei(value: boolean): string {
+  return value ? "Ja" : "Nei"
+}
+
 function arealLinje(a: {
   bolig: number
   annet: number
   totalt: number
 }): string {
   return `${a.bolig} / ${a.annet} / ${a.totalt} m²`
+}
+
+function erFerdigstilt(endring: Bygningsendring): boolean {
+  return (
+    endring.datoer.ferdigattest !== null || endring.datoer.tattIBruk !== null
+  )
+}
+
+function utgattKode(endring: Bygningsendring): string {
+  return endring.bygningsstatus.bestaaende
+    ? "-"
+    : endring.bygningsstatus.kortkode
+}
+
+function minner(endring: Bygningsendring): string {
+  const harSefrak = endring.kulturminner.some(
+    (k) => k.kategori === SEFRAK_KATEGORI,
+  )
+  const harEnkelt = endring.kulturminner.some(
+    (k) => k.kategori !== SEFRAK_KATEGORI,
+  )
+  return `${jaNei(harSefrak)} / ${jaNei(harEnkelt)}`
 }
 
 export function Bygningslinje({ bygning }: Props) {
@@ -58,56 +84,37 @@ export function Bygningslinje({ bygning }: Props) {
           </Table.Row>
         </Table.Head>
         <Table.Body>
-          {bygning.endringer.map((endring) => {
-            const erFerdigstilt =
-              endring.datoer.ferdigattest !== null ||
-              endring.datoer.tattIBruk !== null
-            const utgattKode = endring.bygningsstatus.bestaaende
-              ? "-"
-              : endring.bygningsstatus.kortkode
-            const harSefrakminner = endring.kulturminner.some(
-              (k) => k.kategori === SEFRAK_KATEGORI,
-            )
-            const harEnkeltminner = endring.kulturminner.some(
-              (k) => k.kategori !== SEFRAK_KATEGORI,
-            )
-
-            return (
-              <Table.Row key={endring.id}>
-                <Table.Cell>{bygning.bygningsnr}</Table.Cell>
-                <Table.Cell>{endring.lopenr}</Table.Cell>
-                <Table.Cell>{endring.endringskode ?? "-"}</Table.Cell>
-                <Table.Cell>{endring.bygningsstatus.navn}</Table.Cell>
-                <Table.Cell>{utgattKode}</Table.Cell>
-                <Table.Cell>{bygning.bygningstype.navn}</Table.Cell>
-                <Table.Cell>{bygning.bygningstype.kode}</Table.Cell>
-                <Table.Cell>{bygning.naeringsgruppe}</Table.Cell>
-                <Table.Cell>{endring.datoer.rammetillatelse ?? "-"}</Table.Cell>
-                <Table.Cell>
-                  {endring.datoer.igangsettingstillatelse ?? "-"}
-                </Table.Cell>
-                <Table.Cell>
-                  {endring.datoer.midlertidigBrukstillatelse ?? "-"}
-                </Table.Cell>
-                <Table.Cell>{endring.datoer.ferdigattest ?? "-"}</Table.Cell>
-                <Table.Cell>{endring.datoer.tattIBruk ?? "-"}</Table.Cell>
-                <Table.Cell>{endring.datoer.utgaattRevet ?? "-"}</Table.Cell>
-                <Table.Cell>{arealLinje(endring.bruksareal)}</Table.Cell>
-                <Table.Cell>{arealLinje(endring.bruttoareal)}</Table.Cell>
-                <Table.Cell>{endring.antallBoenheter}</Table.Cell>
-                <Table.Cell>{formatArea(endring.bebygdAreal)}</Table.Cell>
-                <Table.Cell>
-                  {endring.koordinat.nord}, {endring.koordinat.ost}
-                </Table.Cell>
-                <Table.Cell>{erFerdigstilt ? "Ja" : "Nei"}</Table.Cell>
-                <Table.Cell>
-                  {`${harSefrakminner ? "Ja" : "Nei"} / ${
-                    harEnkeltminner ? "Ja" : "Nei"
-                  }`}
-                </Table.Cell>
-              </Table.Row>
-            )
-          })}
+          {bygning.endringer.map((endring) => (
+            <Table.Row key={endring.id}>
+              <Table.Cell>{bygning.bygningsnr}</Table.Cell>
+              <Table.Cell>{endring.lopenr}</Table.Cell>
+              <Table.Cell>{endring.endringskode ?? "-"}</Table.Cell>
+              <Table.Cell>{endring.bygningsstatus.navn}</Table.Cell>
+              <Table.Cell>{utgattKode(endring)}</Table.Cell>
+              <Table.Cell>{bygning.bygningstype.navn}</Table.Cell>
+              <Table.Cell>{bygning.bygningstype.kode}</Table.Cell>
+              <Table.Cell>{bygning.naeringsgruppe}</Table.Cell>
+              <Table.Cell>{endring.datoer.rammetillatelse ?? "-"}</Table.Cell>
+              <Table.Cell>
+                {endring.datoer.igangsettingstillatelse ?? "-"}
+              </Table.Cell>
+              <Table.Cell>
+                {endring.datoer.midlertidigBrukstillatelse ?? "-"}
+              </Table.Cell>
+              <Table.Cell>{endring.datoer.ferdigattest ?? "-"}</Table.Cell>
+              <Table.Cell>{endring.datoer.tattIBruk ?? "-"}</Table.Cell>
+              <Table.Cell>{endring.datoer.utgaattRevet ?? "-"}</Table.Cell>
+              <Table.Cell>{arealLinje(endring.bruksareal)}</Table.Cell>
+              <Table.Cell>{arealLinje(endring.bruttoareal)}</Table.Cell>
+              <Table.Cell>{endring.antallBoenheter}</Table.Cell>
+              <Table.Cell>{formatArea(endring.bebygdAreal)}</Table.Cell>
+              <Table.Cell>
+                {endring.koordinat.nord}, {endring.koordinat.ost}
+              </Table.Cell>
+              <Table.Cell>{jaNei(erFerdigstilt(endring))}</Table.Cell>
+              <Table.Cell>{minner(endring)}</Table.Cell>
+            </Table.Row>
+          ))}
         </Table.Body>
       </Table>
     </section>
