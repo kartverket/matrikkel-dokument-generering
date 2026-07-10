2f956b3c03171563917de132d4e8e489a2635d29
fredrir <fredrik.hansteen@kartverket.no>
2026-07-10 11:36:57 +0200

    oppdatere variabel navn

--- src/lib/utils/getKulturMinner.ts
+++ src/lib/utils/getKulturMinner.ts
@@ -2,11 +2,11 @@ import { SEFRAK_KATEGORI } from "../constants"
 import type { Bygningsendring } from "../schema/byggRapportSchema"
 
 export function getKulturMinner(endring: Bygningsendring): string {
-  const harSefrak = endring.kulturminner.some(
+  const hasKulturMinnerSefrak = endring.kulturminner.some(
     (k) => k.kategori === SEFRAK_KATEGORI,
   )
-  const harEnkelt = endring.kulturminner.some(
+  const hasKulturMinnerEnkelt = endring.kulturminner.some(
     (k) => k.kategori !== SEFRAK_KATEGORI,
   )
-  return `${harSefrak ? "Ja" : "Nei"} / ${harEnkelt ? "Ja" : "Nei"}`
+  return `${hasKulturMinnerSefrak ? "Ja" : "Nei"} / ${hasKulturMinnerEnkelt ? "Ja" : "Nei"}`
 }
