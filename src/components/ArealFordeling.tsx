0aca43bce47a9104fe11ce3ce7b3a57f34022dd8
fredrir <fredrik.hansteen@kartverket.no>
2026-07-13 11:49:04 +0200

    fjernet tabell header bunn linje

--- src/components/ArealFordeling.tsx
+++ src/components/ArealFordeling.tsx
@@ -14,7 +14,7 @@ export default function Arealfordeling({ endring }: Props) {
 
   return (
     <section className="flex flex-col gap-8 my-16">
-      <Heading level={2} className="font-normal text-kv-subtle">
+      <Heading className="font-normal text-kv-subtle text-lg">
         {t(`${af}.title`)}
       </Heading>
 
@@ -47,12 +47,18 @@ export default function Arealfordeling({ endring }: Props) {
         </dd>
       </dl>
 
-      <Table className="text-kv-default">
+      <Table className="text-kv-subtle text-xl">
         <Table.Head>
           <Table.Row>
-            <Table.HeaderCell>{t(`${af}.etasje`)}</Table.HeaderCell>
-            <Table.HeaderCell>{t(`${af}.bolig`)}</Table.HeaderCell>
-            <Table.HeaderCell>{t(`${af}.annet`)}</Table.HeaderCell>
+            <Table.HeaderCell className="border-b-0">
+              {t(`${af}.etasje`)}
+            </Table.HeaderCell>
+            <Table.HeaderCell className="border-b-0">
+              {t(`${af}.bolig`)}
+            </Table.HeaderCell>
+            <Table.HeaderCell className="border-b-0">
+              {t(`${af}.annet`)}
+            </Table.HeaderCell>
           </Table.Row>
         </Table.Head>
         <Table.Body>
@@ -69,7 +75,7 @@ export default function Arealfordeling({ endring }: Props) {
                       level={3}
                       data-size="sm"
                       asChild
-                      className="font-semibold"
+                      className="font-medium text-kv-default"
                     >
                       <span>{etasje.etasjeplan}</span>
                     </Heading>
@@ -83,12 +89,16 @@ export default function Arealfordeling({ endring }: Props) {
                 </Table.HeaderCell>
               </Table.Row>
               <Table.Row>
-                <Table.Cell>{t(`${af}.bruksarealRad`)}</Table.Cell>
-                <Table.Cell>{etasje.bruksareal.bolig}</Table.Cell>
+                <Table.Cell className="text-kv-default">
+                  {t(`${af}.bruksarealRad`)}
+                </Table.Cell>
+                <Table.Cell className="">{etasje.bruksareal.bolig}</Table.Cell>
                 <Table.Cell>{etasje.bruksareal.annet}</Table.Cell>
               </Table.Row>
               <Table.Row>
-                <Table.Cell>{t(`${af}.bruttoarealRad`)}</Table.Cell>
+                <Table.Cell className="text-kv-default">
+                  {t(`${af}.bruttoarealRad`)}
+                </Table.Cell>
                 <Table.Cell>{etasje.bruttoareal.bolig}</Table.Cell>
                 <Table.Cell>{etasje.bruttoareal.annet}</Table.Cell>
               </Table.Row>
