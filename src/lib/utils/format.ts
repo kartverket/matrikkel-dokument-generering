eb1134b96f18879a5d5a343f851065e9c9e2153d
fredrir <fredrik.hansteen@kartverket.no>
2026-07-13 15:01:43 +0200

    fix merge

--- src/lib/utils/format.ts
+++ src/lib/utils/format.ts
@@ -2,10 +2,7 @@ export function formatArea(value: number) {
   return `${value} m²`
 }
 
-export function formatDateTime(
-  value: string | number | Date,
-  locale: string,
-) {
+export function formatDateTime(value: string | number | Date, locale: string) {
   return new Intl.DateTimeFormat(locale, {
     dateStyle: "long",
     timeStyle: "short",
