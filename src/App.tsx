9f073e79d069ade4efec0fd249ecf2a562d2fc84
Fredrik Hansteen <fredrik.hansteen@kartverket.no>
2026-07-15 12:42:14 +0200

    La til mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { I18nextProvider } from "react-i18next"
-import { DocumentComponent } from "./Document.tsx"
-import { createI18n } from "./lib/i18n/createI18n.ts"
-import type { ByggRapport } from "./lib/schema/byggRapportSchema.ts"
-import "./index.css"
+import { I18nextProvider } from "react-i18next";
+import { DocumentComponent } from "./Document.tsx";
+import { createI18n } from "./lib/i18n/createI18n.ts";
+import type { ByggRapport } from "./lib/schema/byggRapportSchema.ts";
+import "./index.css";
 
-const mockModules = import.meta.glob<{ default: ByggRapport }>(
-  "./mockData/*.ts",
-  { eager: true },
-)
-const mockData = Object.values(mockModules)[0]?.default
+const mockModules = import.meta.glob<{ default: ByggRapport }>("./mock/*.ts", {
+  eager: true,
+});
+const mockData = Object.values(mockModules)[0]?.default;
 
 function App() {
   if (!mockData) {
@@ -16,15 +15,15 @@ function App() {
       <section className="flex h-screen w-screen items-center justify-center font-bold text-2xl text-red-700">
         <h1>Ingen mockdata funnet</h1>
       </section>
-    )
+    );
   }
 
-  const i18n = createI18n(mockData.locale)
+  const i18n = createI18n(mockData.locale);
   return (
     <I18nextProvider i18n={i18n}>
       <DocumentComponent data={mockData} />
     </I18nextProvider>
-  )
+  );
 }
 
-export default App
+export default App;
