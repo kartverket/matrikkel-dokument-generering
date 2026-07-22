7da27d54a1647b12fcba769c33b6b7957e74ee6a
Fredrik Hansteen <fredrik.hansteen@kartverket.no>
2026-07-22 12:21:02 +0200

    update mock

--- src/mock/byggRapport.ts
+++ src/mock/byggRapport.ts
@@ -103,7 +103,8 @@ function byggEndring({
     lopeNr,
     byggMetaEndring: {
       endringsKode,
-      bygningsType: "111",
+      bygningsStatusKode: "TB",
+      bygningsTypeKode: "111",
       antallBoenheter: 1,
       naeringsgruppe: "Bolig",
     },
