a9ab815cb0a3318a2c2bb209faf1dabc0fd6b790
Fredrik Hansteen <fredrik.hansteen@kartverket.no>
2026-07-22 12:54:59 +0200

    la til bygg historik funksjon som returner arealendringer

--- src/components/byggoversikt/byggHistorikk.ts
+++ src/components/byggoversikt/byggHistorikk.ts
@@ -1,17 +1,51 @@
-import type { TFunction } from "i18next"
 import type { BygningsEndring } from "../../lib/schema/reports/bygg/byg0011/byggEndring.schema.ts"
 import type { BygningsStatusKode } from "../../lib/schema/reports/bygg/koder/byggningsStatusKode.schema.ts"
 import type { EndringsKode } from "../../lib/schema/reports/bygg/koder/endringsKode.schema.ts"
 
-type HistorikkTranslationKey = "rapport.BYG0011.byggoversikt.historikk"
-
-// Sammendrag av Byggets endringer.
-
+// Sammendrag av byggets endringer.
 type ByggHistorikk = {
   byggEndringsKode: EndringsKode
   byggStatusKode: BygningsStatusKode
   dato: string
   datoType: string
+  byggArealEndring: number
 }
 
-export function byggHistorikk(byggEndringer: BygningsEndring[]) {}
+export function byggHistorikk(
+  byggEndringer: BygningsEndring[],
+): ByggHistorikk[] {
+  if (byggEndringer.length === 0) return []
+
+  return byggEndringer.flatMap((endring) => {
+    if (endring === undefined) return []
+
+    const byggEndringsKode = endring.byggMetaEndring?.endringsKode
+    const byggStatusKode = endring.byggMetaEndring?.bygningsStatusKode
+    const byggArealEndring =
+      endring.byggArealEndring?.bruksarealBolig.totaltAreal
+
+    if (
+      byggEndringsKode === undefined ||
+      byggStatusKode === undefined ||
+      byggArealEndring === undefined
+    ) {
+      return []
+    }
+
+    return Object.entries(endring.byggDatoEndring ?? {}).flatMap(
+      ([datoType, dato]) => {
+        if (dato === undefined) return []
+
+        return [
+          {
+            byggEndringsKode,
+            byggStatusKode,
+            dato,
+            datoType,
+            byggArealEndring,
+          },
+        ]
+      },
+    )
+  })
+}
