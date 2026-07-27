0052462d19a4f83513fde7f9145ca08fb040c69d
fredrir <fhansteen@gmail.com>
2026-07-27 10:28:08 +0200

    Satt inn checkboxer for omfangskriterier

--- src/components/utvalgskriterier/OmfangsKriterier.tsx
+++ src/components/utvalgskriterier/OmfangsKriterier.tsx
@@ -1,4 +1,4 @@
-import { Heading, Table } from "@kv-designsystem/react"
+import { Checkbox, Heading } from "@kv-designsystem/react"
 import { useTranslation } from "react-i18next"
 import type { ByggUtvalgskriterier as Utvalgskriterier } from "../../lib/schema/reports/bygg/shared/byggUtvalgskriterier.schema.ts"
 import { erAngitt, harAngittVerdi } from "./utils/erAngitt.ts"
@@ -18,70 +18,38 @@ export function OmfangsKriterier({ omfangsKriterier }: Props) {
       <Heading level={3} data-size="sm" className="mb-4 font-medium">
         {t(`${uk}.omfang.tittel`)}
       </Heading>
-      <Table zebra border className="w-full table-fixed">
-        <Table.Body>
-          {erAngitt(omfangsKriterier?.inkluderBestaaendeBygg) && (
-            <Table.Row>
-              <Table.HeaderCell scope="row" className="w-1/3">
-                {t(`${uk}.omfang.inkluderBestaaendeBygg`)}
-              </Table.HeaderCell>
-              <Table.Cell>
-                {t(
-                  `${uk}.${omfangsKriterier.inkluderBestaaendeBygg ? "ja" : "nei"}`,
-                )}
-              </Table.Cell>
-            </Table.Row>
-          )}
-          {erAngitt(omfangsKriterier?.inkluderUtgaatteBygg) && (
-            <Table.Row>
-              <Table.HeaderCell scope="row" className="w-1/3">
-                {t(`${uk}.omfang.inkluderUtgaatteBygg`)}
-              </Table.HeaderCell>
-              <Table.Cell>
-                {t(
-                  `${uk}.${omfangsKriterier.inkluderUtgaatteBygg ? "ja" : "nei"}`,
-                )}
-              </Table.Cell>
-            </Table.Row>
-          )}
-          {erAngitt(omfangsKriterier?.inkluderBygninger) && (
-            <Table.Row>
-              <Table.HeaderCell scope="row" className="w-1/3">
-                {t(`${uk}.omfang.inkluderBygninger`)}
-              </Table.HeaderCell>
-              <Table.Cell>
-                {t(
-                  `${uk}.${omfangsKriterier.inkluderBygninger ? "ja" : "nei"}`,
-                )}
-              </Table.Cell>
-            </Table.Row>
-          )}
-          {erAngitt(omfangsKriterier?.inkluderBygningsendringer) && (
-            <Table.Row>
-              <Table.HeaderCell scope="row" className="w-1/3">
-                {t(`${uk}.omfang.inkluderBygningsendringer`)}
-              </Table.HeaderCell>
-              <Table.Cell>
-                {t(
-                  `${uk}.${omfangsKriterier.inkluderBygningsendringer ? "ja" : "nei"}`,
-                )}
-              </Table.Cell>
-            </Table.Row>
-          )}
-          {erAngitt(omfangsKriterier?.inkluderFrededeBygninger) && (
-            <Table.Row>
-              <Table.HeaderCell scope="row" className="w-1/3">
-                {t(`${uk}.omfang.inkluderFrededeBygninger`)}
-              </Table.HeaderCell>
-              <Table.Cell>
-                {t(
-                  `${uk}.${omfangsKriterier.inkluderFrededeBygninger ? "ja" : "nei"}`,
-                )}
-              </Table.Cell>
-            </Table.Row>
-          )}
-        </Table.Body>
-      </Table>
+      <div className="flex flex-row w-full justify-between gap-2">
+        {erAngitt(omfangsKriterier?.inkluderBestaaendeBygg) && (
+          <Checkbox
+            label={t(`${uk}.omfang.inkluderBestaaendeBygg`)}
+            checked={omfangsKriterier.inkluderBestaaendeBygg}
+          />
+        )}
+        {erAngitt(omfangsKriterier?.inkluderUtgaatteBygg) && (
+          <Checkbox
+            label={t(`${uk}.omfang.inkluderUtgaatteBygg`)}
+            checked={omfangsKriterier.inkluderUtgaatteBygg}
+          />
+        )}
+        {erAngitt(omfangsKriterier?.inkluderBygninger) && (
+          <Checkbox
+            label={t(`${uk}.omfang.inkluderBygninger`)}
+            checked={omfangsKriterier.inkluderBygninger}
+          />
+        )}
+        {erAngitt(omfangsKriterier?.inkluderBygningsendringer) && (
+          <Checkbox
+            label={t(`${uk}.omfang.inkluderBygningsendringer`)}
+            checked={omfangsKriterier.inkluderBygningsendringer}
+          />
+        )}
+        {erAngitt(omfangsKriterier?.inkluderFrededeBygninger) && (
+          <Checkbox
+            label={t(`${uk}.omfang.inkluderFrededeBygninger`)}
+            checked={omfangsKriterier.inkluderFrededeBygninger}
+          />
+        )}
+      </div>
     </section>
   )
 }
