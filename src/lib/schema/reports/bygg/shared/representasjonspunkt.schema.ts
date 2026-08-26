e81bb1a92ce2310dc33ec6330c56782dbb33bfc3
Stian Alexander Solli <stian.alexander.solli@kartverket.no>
2026-08-26 14:20:33 +0200

    TH-3130 Fjerner posisjonskvalitet

--- src/lib/schema/reports/bygg/shared/representasjonspunkt.schema.ts
+++ src/lib/schema/reports/bygg/shared/representasjonspunkt.schema.ts
@@ -1,16 +1,11 @@
 import { z } from "@hono/zod-openapi"
 import { koordinatSystemKodeSchema } from "../../../core/koder/koordinatSystemKode.schema.ts"
 import { kodeSchemaOgTekst } from "../../../core/utils/zodUtils.ts"
-import { posisjonskvalitetSchema } from "./posisjonskvalitet.schema.ts"
 
 export const representasjonspunktSchema = z
   .object({
     koordinatsystemKode: kodeSchemaOgTekst(koordinatSystemKodeSchema),
     originalKoordinatsystemKode: kodeSchemaOgTekst(koordinatSystemKodeSchema),
-    kvalitet: posisjonskvalitetSchema.optional(),
-
-    koordinatkvalitetKode: z.string().optional(), // TODO: Fiks kodeverdi for denne?
-
     stedfestingVerifisert: z.boolean().optional(),
 
     nord: z.number().optional(),
